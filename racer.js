e7b7b6513e4170500f0ad9fe7fd530d6b82323d6
James Sangalli <j.l.sangalli@gmail.com>
2016-02-10 12:00:42 +1300

    add files, create base HTML and CSS

---
+++ racer.js
@@ -0,0 +1,14 @@
+function move = function(player){
+	/*
+	psuedo code:
+	
+	if Keypressed = "E"
+	then move first car forward
+	else d move second car forward
+	
+	if player1 has moved to the end
+	then end the game
+	
+	else for player 2
+	*/
+}
