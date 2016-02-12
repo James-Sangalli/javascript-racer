5fdad15d4ced1114a4e375acc4ab226db2edea4c
James Sangalli <j.l.sangalli@gmail.com>
2016-02-12 20:07:34 +1300

    update length settings

--- racer.js
+++ racer.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var trackLength = 10; //10 by default
+  var trackLength = 0; //10 by default
   var p1 = "#player1_strip";
   var p2 = "#player2_strip";
-  // var player1 = document.querySelectorAll("#player1_strip");
-  // var player2 = document.querySelectorAll("#player2_strip");
   var p1Score = 0; 
   var p2Score = 0;
+
+  setLengthTrack();
   
   //var raceTable = document.getElementById("racer_table");
   document.addEventListener("keyup",keyStroke,false); //tracks key strokes
@@ -17,9 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
 			move(p1); //calls function to move player
 			p1Score ++;
 			}
-			else{
+			else if (p1Score >= trackLength){
 				alert("Winner is player 1!");
 				document.removeEventListener("keyup",keyStroke,false); //removes the event listener once the game is over
+				newGame();
 			}
 		} 
 		else if (e.keyCode === 68){ //68 is the letter d
@@ -27,9 +28,10 @@ document.addEventListener('DOMContentLoaded', function() {
 			move(p2);
 			p2Score ++;
 			}
-			else{
+			else if (p2Score >=trackLength){
 				alert("Winner is player 2!");
 				document.removeEventListener("keyup",keyStroke,false); //removes the event listener once the game is over
+				newGame();
 			}
 		}
 		
@@ -39,11 +41,23 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 	
 	function setLengthTrack(){
-		trackLength = prompt("Please choose the length of the track from 10-20");
+		trackLength = parseInt(prompt("Please choose the length of the track, between 5-30"));
+
+		if (trackLength < 5){
+			trackLength === 5;
+		}
+		else if (trackLength > 30){
+			trackLength ===30;
+		}
+
+		// for(i = 0; i < trackLength.length; i ++){
+		// 	trackLength[i] = document.createElement(p1 + "td");
+		// 	trackLength[i] = document.createElement(p2 + "td");
+		// }
 	}
 	
 	function move(player){
-		
+
 		var move = document.querySelector(player + " .active");
 
 		move.classList.remove("active");
@@ -53,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 	
 	function newGame(){
-		window.location.href("main.html"); //resets the page 
+		location.reload();; //resets the page 
 	}
 		
 
