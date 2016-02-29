f03b3defafaf7bc7b7083f6375917c9facfb6673
James Sangalli <j.l.sangalli@gmail.com>
2016-02-29 16:10:35 +1300

    add bacbackground and sound effects

--- racer.js
+++ racer.js
@@ -17,7 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var p1Score = 0; 
   var p2Score = 0;
   var trackLength = 0;
+
   var snd = new Audio("Skid.mp3"); //plays skid noise when user gets a boost
+  var boomSound = new Audio("boom.mp3"); //plays boom sound when car blows
+  var endingCheer = new Audio("Applause.mp3"); //ending cheer sound effect when a player wins
 
   var boost = false; //if player 1 presses x or player 2 presses y they get a 2 step headstart, can only be used once
 
@@ -44,27 +47,29 @@ document.addEventListener('DOMContentLoaded', function() {
 				boost = true;
 				snd.play();
 			}
-
-			randomObstacle(); //calls this, if collision occurs then the game will stop
 		}
 
 		if (e.keyCode === 69){ //69 is the letter e
+			randomObstacle(e.keyCode); //calls this, if collision occurs then the game will stop
 			if(p1Score < trackLength){
 			move(p1); //calls function to move player
 			p1Score ++;
 			}
 			else if (p1Score >= trackLength){
+				endingCheer.play();
 				alert("Tesla Won! Woo hoo!");
 				document.removeEventListener("keyup",keyStroke,false); //removes the event listener once the game is over
 				newGame();
 			}
 		} 
 		else if (e.keyCode === 68){ //68 is the letter d
+			randomObstacle(e.keyCode); //calls this, if collision occurs then the game will stop
 			if(p2Score < trackLength){
 			move(p2);
 			p2Score ++;
 			}
 			else if (p2Score >=trackLength){
+				endingCheer.play();
 				alert("Ford won! Elon Will be pissed!");
 				document.removeEventListener("keyup",keyStroke,false); //removes the event listener once the game is over
 				newGame();
@@ -87,17 +92,18 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	}
 
-	function randomObstacle(){
+	function randomObstacle(key){
 
 		var obstaclePlace = Math.floor(Math.random() *trackLength);
 
-		if (obstaclePlace === p1Score){
+		if (obstaclePlace === p1Score && key == 88){
+			boom.play();
 			alert("Oh NO! Your tesla Caught fire! You lose!");
 			newGame();
 		}
-		
 
-		if (obstaclePlace === p2Score){
+		if (obstaclePlace === p2Score && key == 89){
+			boom.play();
 			alert("Looks like ford really does == Fix Or Repair Daily, your engine has blown and you lose the race!");
 			newGame();
 		}
